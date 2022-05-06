fe85815b7279c4452c3ac3f0fcd54b694edfcd7d
Uree1229 <101373376+Uree1229@users.noreply.github.com>
2022-05-06 11:10:51 +0900

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,1 +1,10 @@
+ObjectID createObject(const char* image, SceneID scene, int x, int y, bool shown) {
 
+	ObjectID object = createObject(image);
+	locateObject(object, scene, x, y);
+
+	if (shown) {
+		showObject(object);
+	}
+	return object;
+}
