b8dfe0e2bad9b79388820059d68ed216c0f99ecb
Uree1229 <101373376+Uree1229@users.noreply.github.com>
2022-05-10 10:19:30 +0900

    Update Stage3.cpp

--- Stage3.cpp
+++ Stage3.cpp
@@ -1,1 +1,39 @@
+#include <bangtal.h> //rhythm game
+
+using namespace std;
+
+TimerID timerA;
+
+int gametype;
+
+void setting4() {
+	gametype = 4;
+	timerA = createTimer(1.0);
+}
+
+void Timer_callback(TimerID timer) {
+	if (timer == timerA) {
+
+	}
+}
+void Keyboard_callback(KeyCode key, KeyState state) {
+	if (gametype == 4) {
+		switch (key) {
+		
+		case KeyCode::KEY_DOWN_ARROW:
+			startTimer(timerA);
+			break;
+		case KeyCode::KEY_UP_ARROW:
+
+			break;
+		case KeyCode::KEY_RIGHT_ARROW:
+
+			break;
+		case KeyCode::KEY_LEFT_ARROW:
+
+			break;
+
+		}
+	}
+}
 
