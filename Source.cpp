ce5857a5a0b3241fa60e102dbd738839b0bf7c65
Uree1229 <101373376+Uree1229@users.noreply.github.com>
2022-05-10 10:40:58 +0900

    5/10(3)

--- Source.cpp
+++ Source.cpp
@@ -5,20 +5,40 @@
 #include "Stage4.h"
 #include "Main.h"
 
+extern int gametype = 0;          // gametype = 0 ∏ﬁ¿Œ, 1 = stage1, 2=stage2, 3=stage3, 4=stage4, 5=bossstage
 SceneID scene,scene1,scene2,scene3,scene4;
-ObjectID door[3];
+ObjectID door[4];
 
 
 void Mouse_callback(ObjectID object, int x, int y, MouseAction action) {
 
-	if(object == door[0]) enterScene(scene1);
-	else if (object == door[1]) enterScene(scene2);
-	else if (object == door[2]) enterScene(scene3);
-	else if (object == door[3]) enterScene(scene4);
+	if (gametype == 0) {
+		if (object == door[0]) {
+			gametype = 1;
+			enterScene(scene1);
+		}
+		else if (object == door[1]) {
+			gametype = 2;
+			enterScene(scene2);
+		}
+		else if (object == door[2]) {
+			gametype = 3;
+			enterScene(scene3);
+		}
+		else if (object == door[3]) {
+			gametype = 4;
+			enterScene(scene4);
+		}
+	}
+
+	
 }
+
+
 
 int main() {
 	setMouseCallback(Mouse_callback);
+	setKeyboardCallback(Keyboard_callback);
 	scene = createScene("main", "Images/back.png");
 	scene1 = createScene("main", "Images/scene1.png");
 	scene2 = createScene("main", "Images/scene2.png");
