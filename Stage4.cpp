4cd0601a7364dbd8fd7a608aeca9fb042a98d03f
이현서 dlgustj1017 <100995874+dlgustj1017@users.noreply.github.com>
2022-06-02 20:45:24 +0900

    Add files via upload

--- Stage4.cpp
+++ Stage4.cpp
@@ -11,35 +11,29 @@ ObjectID INT[6];
 extern ObjectID door[4];
 extern SceneID scene, scene4, scene_stat;
 extern int gametype;
+int correct = 0, wrong = 0, plusINT = 0;
 SceneID scene_1, scene_2, scene_3;
 ObjectID start_stage4, quit, play, fxxk_4,
 category1, category2, category3,
 Mquiz1, Mquiz2, Mquiz3,
 Equiz1, Equiz2, Equiz3,
 Nquiz1, Nquiz2, Nquiz3,
-Pquiz1, Pquiz2, Pquiz3,
 Ma_1_1, Ma_1_2, Ma_1_3,
 Ma_2_1, Ma_2_2, Ma_2_3,
 Ma_3_1, Ma_3_2, Ma_3_3,
-Ma_4_1, Ma_4_2, Ma_4_3,
 Ea_1_1, Ea_1_2, Ea_1_3,
 Ea_2_1, Ea_2_2, Ea_2_3,
 Ea_3_1, Ea_3_2, Ea_3_3,
-Ea_4_1, Ea_4_2, Ea_4_3,
 Na_1_1, Na_1_2, Na_1_3,
 Na_2_1, Na_2_2, Na_2_3,
-Na_3_1, Na_3_2, Na_3_3,
-Na_4_1, Na_4_2, Na_4_3,
-Pa_1_1, Pa_1_2, Pa_1_3,
-Pa_2_1, Pa_2_2, Pa_2_3,
-Pa_3_1, Pa_3_2, Pa_3_3,
-Pa_4_1, Pa_4_2, Pa_4_3;
+Na_3_1, Na_3_2, Na_3_3;
+
 int life_4 = 3;
 
 //stat 함수
 void stat_check_INT()
 {
-    if (life_4 == 3) {
+    if (plusINT == 3) {
         showObject(INT[5]);
         showObject(INT[4]);
         showObject(INT[3]);
@@ -47,17 +41,17 @@ void stat_check_INT()
         showObject(INT[1]);
         showObject(INT[0]);
     }
-    else if (life_4 == 2) {
+    else if (plusINT == 2) {
         showObject(INT[3]);
         showObject(INT[2]);
         showObject(INT[1]);
         showObject(INT[0]);
     }
-    else if (life_4 == 1) {
+    else if (plusINT == 1) {
         showObject(INT[1]);
         showObject(INT[0]);
     }
-    else if (life_4 == 0) {
+    else if (plusINT == 0) {
         showObject(INT[0]);
     }
 }
@@ -70,41 +64,23 @@ void endgame_4()
     locateObject(fxxk_4, scene_stat, 400, 460);
     showObject(fxxk_4);
     enterScene(scene_stat);    
-  
-    
     stat_check_INT();
     gametype = -1;
-    startTimer(timer_cr_4);
-
 }
 
-
-//타이머 콜백 함수
-void Timer_callback_4(TimerID timer)
-{
-    if (timer == timer_cr_4)
-    {
-        enterScene(scene);
-        hideObject(fxxk_4);
-        gametype = 0;
-        setTimer(timer_cr_4, 5.0f);
-    }
-}
 
 
 //콜백함수 정의 
 void mouseCallback_in_stage4(ObjectID object, int x, int y, MouseAction action)
 {
 
-
     //종료 버튼이 눌려지면 stat 결과 나오면서 메인스테이지로 돌아가기
     if (object == quit) 
     {
-        enterScene(scene);
         scene_stat = createScene("scene_stat", "Images/stat.png");
         enterScene(scene_stat);
-        hideObject(fxxk_4);
-        gametype = 0;
+        stat_check_INT();
+        gametype = -1;
         hideObject(door[3]);
     }
 
@@ -122,7 +98,7 @@ void mouseCallback_in_stage4(ObjectID object, int x, int y, MouseAction action)
 
     if (object == category2)
     {
-        enterScene(scene_2);
+        enterScene(scene_1);
         showMessage("여기는 영어 단어 퀴즈 방입니다. 퀴즈를 풀어서 '지식' 능력치를 향상시키세요.");
         showObject(Equiz1);
         showObject(Ea_1_1);
@@ -132,7 +108,7 @@ void mouseCallback_in_stage4(ObjectID object, int x, int y, MouseAction action)
 
     if (object == category3)
     {
-        enterScene(scene_3);
+        enterScene(scene_1);
         showMessage("여기는 넌센스 퀴즈 방입니다. 퀴즈를 풀어서 '지식' 능력치를 향상시키세요.");
         showObject(Nquiz1);
         showObject(Na_1_1);
@@ -141,23 +117,35 @@ void mouseCallback_in_stage4(ObjectID object, int x, int y, MouseAction action)
     }
 
 
-    //수학 퀴즈 방 입장    //퀴즈는 정답 여부에 관계없이 답을 클릭하면 다음문제로 넘어감
-    if (object == Ma_1_1 || object == Ma_1_2 || object == Ma_1_3)
+    //수학 퀴즈 방 입장    
+    if (object == Ma_1_1 || object == Ma_1_2) //퀴즈는 정답 여부에 관계없이 답을 클릭하면 다음문제로 넘어감
     {
-        life_4 -= 1;
+        plusINT -= 1; //정답이 아니면 INT를 -1 한다
 
         hideObject(Ma_1_1);
         hideObject(Ma_1_2);
         hideObject(Ma_1_3);
-
+        hideObject(Mquiz1);
         showObject(Mquiz2);
         showObject(Ma_2_1);
         showObject(Ma_2_2);
         showObject(Ma_2_3);
     }
-    if (object == Ma_2_1 || object == Ma_2_2 || object == Ma_2_3)
+    else if (object == Ma_1_3)
     {
-        life_4 -= 1;
+        hideObject(Ma_1_1);
+        hideObject(Ma_1_2);
+        hideObject(Ma_1_3);
+        hideObject(Mquiz1);
+        showObject(Mquiz2);
+        showObject(Ma_2_1);
+        showObject(Ma_2_2);
+        showObject(Ma_2_3);
+    }
+
+    if (object == Ma_2_1 || object == Ma_2_2)
+    {
+        plusINT -= 1;
 
         hideObject(Ma_2_1);
         hideObject(Ma_2_2);
@@ -168,12 +156,24 @@ void mouseCallback_in_stage4(ObjectID object, int x, int y, MouseAction action)
         showObject(Ma_3_2);
         showObject(Ma_3_3);
     }
-    if (object == Ma_3_1 || object == Ma_3_2 || object == Ma_3_3)
+    else if (object == Ma_2_3)
     {
-        life_4 -= 1;
+        hideObject(Ma_2_1);
+        hideObject(Ma_2_2);
+        hideObject(Ma_2_3);
+        hideObject(Mquiz2);
+        showObject(Mquiz2);
+        showObject(Ma_3_1);
+        showObject(Ma_3_2);
+        showObject(Ma_3_3);
+    }
+    if (object == Ma_3_1 || object == Ma_3_2)
+    {
+        plusINT -= 1;
         hideObject(Ma_3_3);
         hideObject(Ma_3_2);
         hideObject(Ma_3_3);
+        hideObject(Mquiz3);
 
         enterScene(scene4);   //퀴즈를 다 풀면 나가는 방으로 돌아옴
 
@@ -181,13 +181,20 @@ void mouseCallback_in_stage4(ObjectID object, int x, int y, MouseAction action)
         hideObject(category1);
         hideObject(category2);
         hideObject(category3);
+    }
+    else if (object == Ma_3_3)
+    {
+        hideObject(Ma_3_1);
+        hideObject(Ma_3_2);
+        hideObject(Ma_3_3);
+        hideObject(Mquiz3);
 
-        //퀴즈를 다 풀면 나가는 방으로 돌아옴
-        enterScene(scene4);
+        enterScene(scene4);   //퀴즈를 다 풀면 나가는 방으로 돌아옴
+
+        hideObject(start_stage4);
         hideObject(category1);
         hideObject(category2);
         hideObject(category3);
-        hideObject(start_stage4);
     }
 
 
@@ -281,8 +288,6 @@ void mouseCallback_in_stage4(ObjectID object, int x, int y, MouseAction action)
         hideObject(start_stage4);
     }
 
-
-
     //start 버튼을 누르면 
     if (object = start_stage4) 
     {
@@ -305,10 +310,10 @@ int Quiz_stage4() {
     showMessage("퀴즈 방에 오신 것을 환영합니다. 주어지는 퀴즈를 풀어 '지식' 능력치를 향상시키세요.");
 
 
-    //각 퀴즈 방 생성
+    //퀴즈 방 생성
     scene_1 = createScene("math_QUIZ", "Images/mathdungeon.png");
-    scene_2 = createScene("english_QUIZ", "Images/englishdungeon.png");
-    scene_3 = createScene("nonsense_QUIZ", "Images/nonsensedungeon.png");
+    //scene_2 = createScene("english_QUIZ", "Images/englishdungeon.png");
+    //scene_3 = createScene("nonsense_QUIZ", "Images/nonsensedungeon.png");
    
 
     //start 버튼
@@ -341,130 +346,121 @@ int Quiz_stage4() {
     //카테고리별 퀴즈 문제&답 생성하기
     //수학 문제 생성
     Mquiz1 = createObject("Images/Mquiz1.png");
+    scaleObject(Mquiz1, 2.3f);
     locateObject(Mquiz1, scene_1, 500, 250);
     Mquiz2 = createObject("Images/Mquiz2.png");
+    scaleObject(Mquiz2, 2.3f);
     locateObject(Mquiz2, scene_1, 500, 250);
     Mquiz3 = createObject("Images/Mquiz3.png");
+    scaleObject(Mquiz3, 2.3f);
     locateObject(Mquiz3, scene_1, 500, 250);
+    //영어 단어 문제 생성
+    Equiz1 = createObject("Images/Equiz1.png");
+    scaleObject(Equiz1, 2.3f);
+    locateObject(Equiz1, scene_1, 500, 250);
+    Equiz2 = createObject("Images/Equiz2.png");
+    scaleObject(Equiz2, 2.3f);
+    locateObject(Equiz2, scene_1, 500, 250);
+    Equiz3 = createObject("Images/Equiz3.png");
+    scaleObject(Equiz3, 2.3f);
+    locateObject(Equiz3, scene_1, 500, 250);
+    //넌센스 문제 생성
+    Nquiz1 = createObject("Images/Nquiz1.png");
+    scaleObject(Nquiz1, 2.3f);
+    locateObject(Nquiz1, scene_1, 500, 250);
+    Nquiz2 = createObject("Images/Nquiz2.png");
+    scaleObject(Nquiz2, 2.3f);
+    locateObject(Nquiz2, scene_1, 500, 250);
+    Nquiz3 = createObject("Images/Nquiz3.png");
+    scaleObject(Nquiz3, 2.3f);
+    locateObject(Nquiz3, scene_1, 500, 250);
     
     //수학 답 생성
     Ma_1_1 = createObject("Images/ma1-1.png");
-    locateObject(Ma_1_1, scene_1, 400, 200);
-    scaleObject(Ma_1_1, 0.4f);
+    locateObject(Ma_1_1, scene_1, 550, 200);
+    scaleObject(Ma_1_1, 0.5f);
     Ma_1_2 = createObject("Images/ma1-2.png");
-    locateObject(Ma_1_2, scene_1, 700, 200);
-    scaleObject(Ma_1_2, 0.4f);
+    locateObject(Ma_1_2, scene_1, 850, 200);
+    scaleObject(Ma_1_2, 0.5f);
     Ma_1_3 = createObject("Images/ma1-3.png");
-    locateObject(Ma_1_3, scene_1, 1000, 200);
-    scaleObject(Ma_1_3, 0.4f);   
+    locateObject(Ma_1_3, scene_1, 1150, 200);
+    scaleObject(Ma_1_3, 0.5f);   
 
     Ma_2_1 = createObject("Images/ma2-1.png");
-    locateObject(Ma_2_1, scene_1, 400, 200);
-    scaleObject(Ma_2_1, 0.4f);
+    locateObject(Ma_2_1, scene_1, 550, 200);
+    scaleObject(Ma_2_1, 0.5f);
     Ma_2_2 = createObject("Images/ma2-2.png");
-    locateObject(Ma_2_2, scene_1, 700, 200);
-    scaleObject(Ma_2_2, 0.4f);
+    locateObject(Ma_2_2, scene_1, 850, 200);
+    scaleObject(Ma_2_2, 0.5f);
     Ma_2_3 = createObject("Images/ma2-3.png");
-    locateObject(Ma_2_3, scene_1, 1000, 200);
-    scaleObject(Ma_2_3, 0.4f);
+    locateObject(Ma_2_3, scene_1, 1150, 200);
+    scaleObject(Ma_2_3, 0.5f);
 
     Ma_3_1 = createObject("Images/ma3-1.png");
-    locateObject(Ma_3_1, scene_1, 400, 200);
-    scaleObject(Ma_3_1, 0.4f);
+    locateObject(Ma_3_1, scene_1, 550, 200);
+    scaleObject(Ma_3_1, 0.5f);
     Ma_3_2 = createObject("Images/ma3-2.png");
-    locateObject(Ma_3_2, scene_1, 700, 200);
-    scaleObject(Ma_3_2, 0.4f);
+    locateObject(Ma_3_2, scene_1, 850, 200);
+    scaleObject(Ma_3_2, 0.5f);
     Ma_3_3 = createObject("Images/ma3-3.png");
-    locateObject(Ma_3_3, scene_1, 1000, 200);
-    scaleObject(Ma_3_3, 0.4f);
-   
-    Ma_4_1 = createObject("Images/ma4-1.png");
-    locateObject(Ma_4_1, scene_1, 400, 200);
-    scaleObject(Ma_4_1, 0.4f);
-    Ma_4_2 = createObject("Images/ma4-2.png");
-    locateObject(Ma_4_2, scene_1, 700, 200);
-    scaleObject(Ma_4_2, 0.4f);
-    Ma_4_3 = createObject("Images/ma4-3.png");
-    locateObject(Ma_4_3, scene_1, 1000, 200);
-    scaleObject(Ma_4_3, 0.4f);
-
-    //영어 단어 문제 생성
-    Equiz1 = createObject("Images/Equiz1.png");
-    locateObject(Equiz1, scene_2, 500, 250);
-    Equiz2 = createObject("Images/Equiz2.png");
-    locateObject(Equiz1, scene_2, 500, 250);
-    Equiz3 = createObject("Images/Equiz3.png");
-    locateObject(Equiz1, scene_2, 500, 250);
+    locateObject(Ma_3_3, scene_1, 1150, 200);
+    scaleObject(Ma_3_3, 0.5f);
+        
   
     //영어 답 생성
     Ea_1_1 = createObject("Images/ea1-1.png");
-    locateObject(Ea_1_1, scene_2, 400, 200);
+    locateObject(Ea_1_1, scene_1, 550, 200);
     Ea_1_2 = createObject("Images/ea1-2.png");
-    locateObject(Ea_1_1, scene_2, 400, 200);
+    locateObject(Ea_1_1, scene_1, 850, 200);
     Ea_1_3 = createObject("Images/ea1-3.png");
-    locateObject(Ea_1_1, scene_2, 400, 200);
+    locateObject(Ea_1_1, scene_1, 1150, 200);
 
     Ea_2_1 = createObject("Images/ea2-1.png");
-    locateObject(Ea_1_1, scene_2, 400, 200);
+    locateObject(Ea_1_1, scene_1, 550, 200);
     Ea_2_2 = createObject("Images/ea2-2.png");
-    locateObject(Ea_1_1, scene_2, 400, 200);
+    locateObject(Ea_1_1, scene_1, 850, 200);
     Ea_2_3 = createObject("Images/ea2-3.png");
-    locateObject(Ea_1_1, scene_2, 400, 200);
+    locateObject(Ea_1_1, scene_1, 1150, 200);
  
     Ea_3_1 = createObject("Images/ea3-1.png");
-    locateObject(Ea_1_1, scene_2, 400, 200);
+    locateObject(Ea_1_1, scene_1, 550, 200);
     Ea_3_2 = createObject("Images/ea3-2.png");
-    locateObject(Ea_1_1, scene_2, 400, 200);
+    locateObject(Ea_1_1, scene_1, 850, 200);
     Ea_3_3 = createObject("Images/ea3-3.png");
-    locateObject(Ea_1_1, scene_2, 400, 200);   
+    locateObject(Ea_1_1, scene_1, 1150, 200);   
 
-    Ea_4_1 = createObject("Images/ea4-1.png");
-    locateObject(Ea_1_1, scene_2, 400, 200);
-    Ea_4_2 = createObject("Images/ea4-2.png");
-    locateObject(Ea_1_1, scene_2, 400, 200);
-    Ea_4_3 = createObject("Images/ea4-3.png");
-    locateObject(Ea_1_1, scene_2, 400, 200);
-   
-
-    //넌센스 문제 생성
-    Nquiz1 = createObject("Images/Nquiz1.png");
-    locateObject(Nquiz1, scene_3, 500, 250);
-    Nquiz2 = createObject("Images/Nquiz2.png");
-    locateObject(Nquiz1, scene_3, 500, 250);
-    Nquiz3 = createObject("Images/Nquiz3.png");
-    locateObject(Nquiz1, scene_3, 500, 250);
-   
+    
     //넌센스 답 생성
     Na_1_1 = createObject("Images/na1-1.png");
-    locateObject(Na_1_1, scene_3, 400, 200);
+    locateObject(Na_1_1, scene_1, 550, 200);
     Na_1_2 = createObject("Images/na1-2.png");
-    locateObject(Na_1_1, scene_3, 400, 200);
+    locateObject(Na_1_1, scene_1, 850, 200);
     Na_1_3 = createObject("Images/na1-3.png");
-    locateObject(Na_1_1, scene_3, 400, 200);
+    locateObject(Na_1_1, scene_1, 1150, 200);
     
     Na_2_1 = createObject("Images/na2-1.png");
-    locateObject(Na_1_1, scene_3, 400, 200);
+    locateObject(Na_1_1, scene_1, 550, 200);
     Na_2_2 = createObject("Images/na2-2.png");
-    locateObject(Na_1_1, scene_3, 400, 200);
+    locateObject(Na_1_1, scene_1, 850, 200);
     Na_2_3 = createObject("Images/na2-3.png");
-    locateObject(Na_1_1, scene_3, 400, 200);
+    locateObject(Na_1_1, scene_1, 1150, 200);
    
     Na_3_1 = createObject("Images/na3-1.png");
-    locateObject(Na_1_1, scene_3, 400, 200);
+    locateObject(Na_1_1, scene_1, 550, 200);
     Na_3_2 = createObject("Images/na3-2.png");
-    locateObject(Na_1_1, scene_3, 400, 200);
+    locateObject(Na_1_1, scene_1, 850, 200);
     Na_3_3 = createObject("Images/na3-3.png");
-    locateObject(Na_1_1, scene_3, 400, 200);
-   
-    Na_4_1 = createObject("Images/na4-1.png");
-    locateObject(Na_1_1, scene_3, 400, 200);
-    Na_4_2 = createObject("Images/na4-2.png");
-    locateObject(Na_1_1, scene_3, 400, 200);
-    Na_4_3 = createObject("Images/na4-3.png");
-    locateObject(Na_1_1, scene_3, 400, 200);
+    locateObject(Na_1_1, scene_1, 1150, 200);
+
+
+
+    for (int i = 0; i < 5; i++)
+    {
+        INT[i] = createObject("Images/INT.png");
+        locateObject(INT[i], scene_stat, 204 + i * 137, 137);
+    }
 
     //startGame(scene4);
-
 
     return 0;
 }
