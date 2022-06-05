1ebb0a9ce1b2baa83f71732156be1b7378c836ef
DogJHDOG <100814191+DogJHDOG@users.noreply.github.com>
2022-06-05 15:44:21 +0900

    Update StageBoss.cpp

--- StageBoss.cpp
+++ StageBoss.cpp
@@ -1,7 +1,7 @@
 #include <bangtal.h>
 
 TimerID timer_b, timer_hp, Timer_sk1,Timer_sk2, Timer_sk3,Timer_range, timer_band, timer_stop, Timer_sk4;
-TimerID j_Timer_b, hitbox_Timer_b, h_Timer, timer_move_b;
+TimerID j_Timer_b, hitbox_Timer_b, h_Timer, timer_move_b,boss_Jump_Timer,boss_Walk_Timer;
 ObjectID hp_p[4], Player, range1,range2,range3, hp_b[100],hit_range_b[5],hit_box_b,rage, rage1;
 extern SceneID sceneB;
 extern int gametype, dex, luk, str, Int;
@@ -11,9 +11,51 @@ using namespace std;
 
 int HP_p = 3, HP_b = 30, type=0, Player_x=0, Player_y=0,ply=120,sk1_x, sk1_y,sk2_x,sk2_y,num_1=0,num_2=0, num_3=0;
 
-int vel_ch_b,hit_xb,hit_yb,x2_b,dx_b;
+int vel_ch_b,hit_xb,hit_yb,x2_b,dx_b,walkidx,walkL,walkR,jumpL,jumpR,jumpidx;
 bool c_jump_b = false;
-bool Rage=false, check1, check2, check3, check4, check0 = false;
+bool Rage=false, check1, check2, check3, check4, check0,walkLb,walkRb,dont_move = false;
+
+char boss_JumpL[20][100]{
+	   "images/R_J1.png",
+	   "images/R_J2.png",
+	   "images/R_J3.png",
+	   "images/R_J4.png",
+	   "images/R_J5.png",
+	   "images/R_J6.png",
+	   "images/R_J7.png"
+
+};
+
+char boss_JumpR[20][100]{
+	   "images/L_J1.png",
+	   "images/L_J2.png",
+	   "images/L_J3.png",
+	   "images/L_J4.png",
+	   "images/L_J5.png",
+	   "images/L_J6.png",
+	   "images/L_J7.png"
+
+};
+
+char boss_WalkR[6][20]{
+		"images/W_R1.png",
+		"images/W_R2.png",
+		"images/W_R3.png",
+		"images/W_R4.png",
+		"images/W_R5.png",
+		"images/W_R6.png"
+};
+
+char boss_WalkL[6][20]{
+		"images/W_L1.png",
+		"images/W_L2.png",
+		"images/W_L3.png",
+		"images/W_L4.png",
+		"images/W_L5.png",
+		"images/W_L6.png"
+
+};
+
 
 
 void boss_skill_1();
@@ -31,8 +73,10 @@ void set_time() {
 	j_Timer_b = createTimer(0.06f);
 	Timer_range = createTimer(0.5f);
 	timer_band = createTimer(1.0f);
-	timer_stop = createTimer(1.0f); // ºˆ¡§
+	timer_stop = createTimer(1.0f); // ÏàòÏ†ï
 	hitbox_Timer_b = createTimer(0.06f);
+	boss_Walk_Timer = createTimer(0.06f);
+	boss_Jump_Timer = createTimer(0.06f);
 
 	
 
@@ -80,7 +124,7 @@ void boss_attack() {
 	
 void setting_B() {
 	if (gametype ==5) {
-		showMessage("aaa"); //∫∏Ω∫¿« ¿‘¿Â¥ÎªÁ
+		showMessage("aaa"); //Î≥¥Ïä§Ïùò ÏûÖÏû•ÎåÄÏÇ¨
 		set_time();
 		for (int i = 0; i < 3; i++) {
 			hp_p[i] = createObject("Images/Heart.png", sceneB, 30 + 70 * i, 50, true);
@@ -98,7 +142,7 @@ void setting_B() {
 
 		startTimer(timer_stop);
 
-		 // ¡ˆøˆæﬂ«‘
+		 // ÏßÄÏõåÏïºÌï®
 	}
 	
 }
@@ -218,7 +262,7 @@ void hp_player() {
 		hideObject(hp_p[1]);
 	}
 }
-//æ¯ø°æﬂµ 
+//ÏóÜÏóêÏïºÎê®
 void hp_boss() {
 
 	int i = 100-HP_b;
@@ -228,7 +272,7 @@ void hp_boss() {
 	
 }
 
-void boss_skill_1() { //F π¸¿ß ∞¯∞› 100,100
+void boss_skill_1() { //F Î≤îÏúÑ Í≥µÍ≤© 100,100
 	if (type == 1) {
 		Int = 50;
 		sk1_x = random_number(0, 1280), sk1_y = random_number(60, 80);
@@ -240,7 +284,7 @@ void boss_skill_1() { //F π¸¿ß ∞¯∞› 100,100
 		
 	}	
 }
-void boss_skill_2() { // ¡°«¡∑Œ «««œ¥¬ ∞≈
+void boss_skill_2() { // Ï†êÌîÑÎ°ú ÌîºÌïòÎäî Í±∞
 	if (type == 2) {
 		sk2_x = random_number(0,1250), sk2_y = random_number(0, 29);
 		range2 = createObject("Images/range1.png", sceneB, 0, sk2_y, false);
@@ -270,14 +314,14 @@ void boss_skill_3() {
 		num_3++;
 	}
 }
-void boss_skill_4() {// «„±ÎªÛ≈¬
+void boss_skill_4() {// ÌóàÍπÖÏÉÅÌÉú
 	show_boss_hitbox();
 	type = 0;
 }
 void ending() {
 	if (HP_b ==0) {
 		showMessage("Happy Ending");
-		//enterScene(); º∫¿˚«• ∆‰¿Ã¡ˆ
+		//enterScene(); ÏÑ±Ï†ÅÌëú ÌéòÏù¥ÏßÄ
 	}
 	else if (HP_p == 0) {
 		showMessage("Bad Ending");
@@ -320,28 +364,49 @@ void Mouse_callback_B(ObjectID object, int x, int y, MouseAction action) {
 	}
 }
 
-void Keyboard_callback_B(KeyCode code, KeyState state) { // default = 0,0 -> ≈©±‚ 70, 120
+void Keyboard_callback_B(KeyCode code, KeyState state) { // default = 0,0 -> ÌÅ¨Í∏∞ 70, 120
 	if (gametype == 5) {
-		if (code == KeyCode::KEY_DOWN_ARROW && ply == 120 &&c_jump_b == false) { //º˜¿Ã±‚  -  º˜¿Ã∏È ≥Ù¿Ã π›¿∏∑Œ §°§°
+		if (code == KeyCode::KEY_DOWN_ARROW && ply == 120 && c_jump_b == false) { //ÏàôÏù¥Í∏∞  -  ÏàôÏù¥Î©¥ ÎÜíÏù¥ Î∞òÏúºÎ°ú „Ñ±„Ñ±
 			ply = 60;
-			setObjectImage(Player, "Images/Heart.png");      // º˜¿Ã¥¬ ¿ÃπÃ¡ˆ ≥÷æÓæﬂ«‘
+			setObjectImage(Player, "Images/S2.png");      // ÏàôÏù¥Îäî Ïù¥ÎØ∏ÏßÄ ÎÑ£Ïñ¥ÏïºÌï®
+			setTimer(boss_Walk_Timer, 0.06f);
+			stopTimer(boss_Walk_Timer);
 			Player_y = 0;
+			dont_move = true;
 		}
 		else if (code == KeyCode::KEY_UP_ARROW && ply == 60) {
 			ply = 120;
-			setObjectImage(Player, "Images/ch1.png");
+			//setObjectImage(Player, "Images/ch1.png");
+			startTimer(boss_Walk_Timer);
 			Player_y = 0;
+			dont_move = false;
 		}
-		else if (code == KeyCode::KEY_LEFT_ARROW) {// ¡¬ ¿Ãµø dex∑Œ Ω∫««µÂ ¡∂¡§
-			dx_b -= (state == KeyState::KEY_PRESSED ? (10 + 1*dex) : -(10 + 1 * dex)); //ºˆ¡§ 
+		else if (code == KeyCode::KEY_LEFT_ARROW && dont_move == false) {// Ï¢å Ïù¥Îèô dexÎ°ú Ïä§ÌîºÎìú Ï°∞Ï†ï
+				if (walkLb == false && walkLb == false && c_jump_b == false) {
+					walkLb = true;
+					walkidx = 1;
+					setTimer(boss_Walk_Timer, 0.06f);
+					startTimer(boss_Walk_Timer);
+				}
+			dx_b -= (state == KeyState::KEY_PRESSED ? (10 + 1*dex) : -(10 + 1 * dex));//ÏàòÏ†ï 
+
 		}
-		else if (code == KeyCode::KEY_RIGHT_ARROW) { //øÏ ¿Ãµø
+
+		else if (code == KeyCode::KEY_RIGHT_ARROW && dont_move == false) { //Ïö∞ Ïù¥Îèô
+				if (walkRb == false&&walkLb == false && c_jump_b == false) {
+					walkRb = true;
+					walkidx = 0;
+					setTimer(boss_Walk_Timer, 0.06f);
+					startTimer(boss_Walk_Timer);
+				}
 			dx_b += (state == KeyState::KEY_PRESSED ? (10 + 1 * dex) : -(10 + 1 * dex));
+
 		}
-		else if (code == KeyCode::KEY_SPACE && c_jump_b == false) {// ¡°«¡ ∞Ì¡§ y +50
+		else if (code == KeyCode::KEY_SPACE && c_jump_b == false) {// Ï†êÌîÑ Í≥†Ï†ï y +50
 			vel_ch_b = 60;
 			c_jump_b = true;
 			startTimer(j_Timer_b);
+			startTimer(boss_Jump_Timer);
 		}
 		else  if (code == KeyCode::KEY_R or code == KeyCode::KEY_CAPITAL_R) {
 			hit_yb = 100;
@@ -375,7 +440,7 @@ void Timer_callback_B(TimerID timer) {
 				HP_p--;
 			}
 		}
-		else if (timer == Timer_sk2 && type ==2) {
+		else if (timer == Timer_sk2 && type == 2) {
 			setObjectImage(range2, "Images/E.png");
 			setObjectImage(range3, "Images/E-1.png");
 			setTimer(Timer_sk2, 1.0f);
@@ -394,7 +459,7 @@ void Timer_callback_B(TimerID timer) {
 				HP_p--;
 			}
 		}
-		else if(timer == Timer_sk4){
+		else if (timer == Timer_sk4) {
 			type = 0;
 			boss_attack();
 			setTimer(Timer_sk4, 1.0f);
@@ -411,12 +476,99 @@ void Timer_callback_B(TimerID timer) {
 				stopTimer(j_Timer_b);
 				c_jump_b = false;
 			}
-		}	
+		}
 		else if (timer == hitbox_Timer_b) {
 			check_hit_b();
 			hideObject(hit_box_b);
 			setTimer(hitbox_Timer_b, 0.06f);
 		}
+
+		else if (timer == boss_Walk_Timer) {
+			if (walkidx == 1) {
+				setObjectImage(Player, boss_WalkL[walkL]);
+				walkL++;
+				setTimer(boss_Walk_Timer, 0.06f);
+				startTimer(boss_Walk_Timer);
+				if (walkL == 5) {
+					walkL = 0;
+					setTimer(boss_Walk_Timer, 0.06f);
+					startTimer(boss_Walk_Timer);
+					walkRb = false;
+					walkLb = false;
+				}
+
+			}
+			else {
+				setObjectImage(Player, boss_WalkR[walkR]);
+				walkR++;
+				setTimer(boss_Walk_Timer, 0.06f);
+				startTimer(boss_Walk_Timer);
+				if (walkR == 5) {
+					walkR = 0;
+					setTimer(boss_Walk_Timer, 0.06f);
+					startTimer(boss_Walk_Timer);
+					walkRb = false;
+					walkLb = false;
+
+				}
+			}
+		}
+
+		else if (timer == boss_Jump_Timer) {
+			setTimer(boss_Walk_Timer, 0.06f);
+			stopTimer(boss_Walk_Timer);
+			if (walkidx == 1) {
+				if (jumpidx < 1) {
+					setObjectImage(Player, boss_JumpL[0]);
+
+				}
+				if (jumpidx >= 1 && jumpidx < 2) {
+					setObjectImage(Player, boss_JumpL[3]);
+
+				}
+				if (jumpidx >= 2) {
+					setObjectImage(Player, boss_JumpL[5]);
+
+				}
+				jumpidx++;
+				setTimer(boss_Jump_Timer, 0.06f);
+				startTimer(boss_Jump_Timer);
+				if (Player_y < 10) {
+					jumpidx = 0;
+					setTimer(boss_Jump_Timer, 0.06f);
+					stopTimer(boss_Jump_Timer);
+					setTimer(boss_Walk_Timer, 0.06f);
+					startTimer(boss_Walk_Timer);
+
+				}
+			}
+			else {
+				if (jumpidx < 1) {
+					setObjectImage(Player, boss_JumpL[0]);
+
+				}
+				if (jumpidx >= 1 && jumpidx < 2) {
+					setObjectImage(Player, boss_JumpL[3]);
+
+				}
+				if (jumpidx >= 2) {
+					setObjectImage(Player, boss_JumpL[5]);
+
+				}
+				jumpidx++;
+				setTimer(boss_Jump_Timer, 0.06f);
+				startTimer(boss_Jump_Timer);
+				if (Player_y < 10) {
+					jumpidx = 0;
+					setTimer(boss_Jump_Timer, 0.06f);
+					stopTimer(boss_Jump_Timer);
+					setTimer(boss_Walk_Timer, 0.06f);
+					startTimer(boss_Walk_Timer);
+
+				}
+			}
+		}
+
 		else if (timer == Timer_range) {
 
 			setTimer(Timer_range, 0.5f);
@@ -440,21 +592,21 @@ void Timer_callback_B(TimerID timer) {
 				}
 				else boss_skill_2();
 			}
-			else if (type ==3) {
+			else if (type == 3) {
 				hideObject(range1);
 				hideObject(range2);
 				hideObject(range3);
-				if (num_3== (1)) { //20 - Int * 2
+				if (num_3 == (1)) { //20 - Int * 2
 					type = 4;
 					num_3 = 0;
 					boss_attack();
 				}
 				else {
 					boss_skill_3();
-					
+
 				}
 			}
-		
+
 
 		}
 		else if (timer_stop) {
@@ -464,5 +616,4 @@ void Timer_callback_B(TimerID timer) {
 		}
 
 	}
-	
 }
